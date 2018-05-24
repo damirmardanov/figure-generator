5d024d46a97d540f7a2b6776edcc3c9f7979b8ec
Damir <damirmaratovich.mardanov@gmail.com>
2018-05-24 16:49:16 +0300

    Added primary inputs and menu

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ from figures.Popcorn import PopcornBuilder
 from tkinter import *
 
 
-def process(self):
+def process():
     # global root
     logs = Logger('log_' + datetime.strftime(datetime.now(), "%d_%m_%H_%M") + '.txt')
     writer = Writer(input("Input csv file name: "))
@@ -106,16 +106,86 @@ def process(self):
 class Application(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.bar_input = Entry(self,
-                               text="input bar count")
-        self.start_button = Button(self,  # родительское окно
-                                   text="Click me",  # надпись на кнопке
-                                   width=30, height=5,  # ширина и высота
-                                   bg="white", fg="black", command=self.on_button)  # цвет фона и надписи
+        input_paddingy = 3
+        label_paddingx = 5
+        input_paddingx = 5
+        self.bar_count = StringVar()
+        self.circle_count = StringVar()
+        self.ellipse_count = StringVar()
+        self.popcorn_count = StringVar()
+        self.dots_count = StringVar()
+        self.res_h = StringVar()
+        self.res_w = StringVar()
+        self.file = StringVar()
+        self._root().title("Генератор обучающей выборки")
+        self.bar_input = Entry(self, textvariable=self.bar_count)
+        self.circle_input = Entry(self, textvariable=self.circle_count)
+        self.dots_input = Entry(self, textvariable=self.ellipse_count)
+        self.ellipse_input = Entry(self, textvariable=self.popcorn_count)
+        self.popcorn_input = Entry(self, textvariable=self.dots_count)
+        self.resolution_h_input = Entry(self, textvariable=self.res_h)
+        self.resolution_w_input = Entry(self, textvariable=self.res_w)
+        self.file_input = Entry(self, textvariable=self.file)
 
-        self.start_button.pack()  # расположить кнопку на главном окне
-        self.bar_input.pack()
-        Button(self, text="Quit", command=self.destroy).pack()
+        self.bar_count.set("0")
+        self.circle_count.set("0")
+        self.dots_count.set("0")
+        self.ellipse_count.set("0")
+        self.popcorn_count.set("0")
+        self.res_h.set("27")
+        self.res_w.set("27")
+        self.file.set("training.csv")
+
+        menubar=Menu(self)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Выход", command=self.destroy)
+
+        aboutmenu = Menu(menubar, tearoff=0)
+        aboutmenu.add_command(label="Помощь", command=self.destroy)
+        aboutmenu.add_command(label="О программе", command=self.destroy)
+
+        menubar.add_cascade(label="Файл", menu=filemenu)
+        menubar.add_cascade(label="О программе", menu=aboutmenu)
+        self.config(menu=menubar)
+
+        self.start_button = Button(self,
+                                   text="Начать генерацию",
+                                   width=20, height=2,
+                                   bg="white", fg="black", command=process)
+
+        Label(self, text='Количество генерируемых объектов округлой формы":')\
+            .grid(row=0, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.circle_input.grid(row=0, column=1, padx=input_paddingx, sticky="w")
+
+        Label(self, text='Количество генерируемых объектов овальной формы:')\
+            .grid(row=1, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.ellipse_input.grid(row=1, column=1, padx=input_paddingx, sticky="w")
+
+        Label(self, text='Количество генерируемых объектов "Стержень":')\
+            .grid(row=2, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.bar_input.grid(row=2, column=1, padx=input_paddingx, sticky="w")
+
+        Label(self, text='Количество генерируемых объектов "Попкорн":')\
+            .grid(row=3, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.popcorn_input.grid(row=3, column=1, padx=input_paddingx, sticky="w")
+
+        Label(self, text='Количество генерируемых объектов "Скопление точек":')\
+            .grid(row=4, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.dots_input.grid(row=4, column=1, padx=input_paddingx, sticky="w")
+
+        Label(self, text='Высота генерируемых изображений:')\
+            .grid(row=5, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.resolution_h_input.grid(row=5, column=1, padx=input_paddingx, sticky="w")
+
+        Label(self, text='Ширина генерируемых изображений:')\
+            .grid(row=6, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.resolution_w_input.grid(row=6, column=1, padx=input_paddingx, sticky="w")
+
+        Label(self, text='Сохранить в:')\
+            .grid(row=7, column=0, padx=label_paddingx, pady=input_paddingy, sticky="e")
+        self.file_input.grid(row=7, column=1, padx=input_paddingx, sticky="w")
+
+        self.start_button.grid(row=8, columnspan=2, pady=input_paddingy)
 
     def on_button(self):
         print(self.bar_input.get())
